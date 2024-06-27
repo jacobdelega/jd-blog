bf2c313a96b7e79e458c43144967364c9090499d
Jacob Delega <jacobdelega@gmail.com>
2024-06-26 21:57:14 -0400

    Completed responsive contact form

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,17 +1,51 @@
 import React from "react";
 import styles from "./contact.module.css";
+import Link from "next/link";
+
+import { FaPerson } from "react-icons/fa6";
 
 const AboutPage = () => {
     return (
         <div className={styles.container}>
-            <h1 className={styles.title}>Contact page</h1>
-            <div className={styles.content}>
-                <p>
-                    work in progress
-                </p>
+            <p className={styles.label}>Contact me</p>
+            <h1 className={styles.title}> Lets get in touch. </h1>
+            <p className={styles.subtitle}>
+                Or just reach out manually to{" "}
+                <Link
+                    href=''
+                    className={styles.email}>
+                    jacobdelega@gmail.com
+                </Link>
+            </p>
+
+            <div className={styles.mainContent}>
+                <div className={styles.inputContainer}>
+                    <label className={styles.inputLabel}>Fullname</label>
+                    <input
+                        type='text'
+                        placeholder='Name'
+                        className={styles.input}
+                    />
+                </div>
+                <div className={styles.inputContainer}>
+                    <label className={styles.inputLabel}>Email</label>
+                    <input
+                        type='email'
+                        placeholder='Email'
+                        className={styles.input}
+                    />
+                </div>
+                <div className={styles.inputContainer}>
+                    <label className={styles.inputLabel}>Message</label>
+                    <textarea
+                        placeholder='Message'
+                        className={styles.textarea}
+                    />
+                </div>
+                <button className={styles.button}>Submit Form </button>
             </div>
         </div>
     );
-}
+};
 
 export default AboutPage;
